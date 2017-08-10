76c0b31c727a5941ae97dae046601ed63c769a5a
sksat <sksat.tec@gmail.com>
2017-08-10 19:08:01 +0900

    [FIX] shell cmd

--- install.sh
+++ install.sh
@@ -2,19 +2,22 @@
 
 # default package manager
 pman=apt
+install_opt=install
 
-get_pacman () {
+get_pman () {
   echo -n "input package manager : "
   read pman
-  echo "package manager : $pman"
+  echo -n "input install option : "
+  read install_opt
+  #echo "package manager : $pman"
 }
 
 if [ '$(uname)' == 'Darwin' ]; then
   pman=brew
-  which || get_pman
+  which $pman || get_pman
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   pman=apt-get
-  which || get_pman
+  which $pman || get_pman
 elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
   pman=choco
   where $pman || @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
@@ -24,5 +27,5 @@ else
 fi
 
 # install packages
-$pman install vim
+$pman $install_opt vim
 
